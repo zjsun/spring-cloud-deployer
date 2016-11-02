8795c333a667a8d01668ebc39c51c215d323f3ea
Mark Pollack <mpollack@pivotal.io>
2016-11-02 16:46:31 -0400

    GH-120 Add PATH env-var to list of inherited env-vars in launched process

--- spring-cloud-deployer-local/src/main/java/org/springframework/cloud/deployer/spi/local/LocalDeployerProperties.java
+++ spring-cloud-deployer-local/src/main/java/org/springframework/cloud/deployer/spi/local/LocalDeployerProperties.java
@@ -50,7 +50,7 @@ public class LocalDeployerProperties {
 	 * Array of regular expression patterns for environment variables that
 	 * should be passed to launched applications.
 	 */
-	private String[] envVarsToInherit = { "TMP", "LANG", "LANGUAGE", "LC_.*" };
+	private String[] envVarsToInherit = { "TMP", "LANG", "LANGUAGE", "LC_.*", "PATH" };
 
 	/**
 	 * The command to run java.
