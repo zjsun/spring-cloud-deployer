599710babfc1805a284a4d1803532a2d3fd1a6c4
Thomas Risberg <trisberg@pivotal.com>
2017-01-04 13:19:18 -0500

    Make TaskStatus.getAttributes public
    Resolves #168

--- spring-cloud-deployer-spi/src/main/java/org/springframework/cloud/deployer/spi/task/TaskStatus.java
+++ spring-cloud-deployer-spi/src/main/java/org/springframework/cloud/deployer/spi/task/TaskStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,7 +97,7 @@ public class TaskStatus {
 	 *
 	 * @return map of attributes for the task
 	 */
-	Map<String, String> getAttributes() {
+	public Map<String, String> getAttributes() {
 		return this.attributes;
 	}
 }
